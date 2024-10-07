6198b5d54b62c7c43e1b0c9ef522643cbd52c1ec
boiledegg <tjrwns2163@naver.com>
2024-10-08 00:34:03 +0900

    ui/#3: 회원가입 ui 구현

---
+++ app/src/main/java/org/sopt/and/presentation/signup/SignUpScreen.kt
@@ -0,0 +1,196 @@
+package org.sopt.and.presentation.signup
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.core.designsystem.component.textfield.ShowActionTextField
+import org.sopt.and.core.designsystem.component.textfield.WavveBasicTextField
+import org.sopt.and.core.designsystem.component.topbar.CancelTopBar
+import org.sopt.and.core.type.AccountType
+import kotlin.text.Typography.bullet
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit = {}
+) {
+    var emailText by remember { mutableStateOf("") }
+    var passwordText by remember { mutableStateOf("") }
+    val commonModifier = Modifier.padding(horizontal = 5.dp)
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = buildAnnotatedString {
+                append("이메일과 비밀번호만으로\nWavve를 즐길 수 있어요!")
+                addStyle(
+                    style = SpanStyle(color = Color.White),
+                    start = 0,
+                    end = 9
+                )
+                addStyle(
+                    style = SpanStyle(color = Color.Gray),
+                    start = 9,
+                    end = 12
+                )
+                addStyle(
+                    style = SpanStyle(color = Color.White),
+                    start = 13,
+                    end = 24
+                )
+                addStyle(
+                    style = SpanStyle(color = Color.Gray),
+                    start = 25,
+                    end = 29
+                )
+            },
+            fontSize = 20.sp,
+            modifier = commonModifier.padding(start = 10.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        WavveBasicTextField(
+            hint = "wavve@example.com",
+            onValueChange = { emailText = it },
+            value = emailText,
+            cursorBrush = SolidColor(Color.Blue),
+            modifier = commonModifier
+        )
+
+        Text(
+            text = "로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을 입력해주세요",
+            color = Color.Gray,
+            fontSize = 12.sp,
+            modifier = commonModifier
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        ShowActionTextField(
+            hint = "Wavve 비밀번호 설정",
+            value = passwordText,
+            onValueChange = { passwordText = it },
+            modifier = commonModifier
+        )
+        Text(
+            text = "비밀번호는 8~20자 이내로 영문 대소문자, 숫자, 특수문자 중 3가지 이상 혼용하여 입력해주세요.",
+            color = Color.Gray,
+            fontSize = 12.sp,
+            modifier = commonModifier
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 40.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .height(0.5.dp)
+                    .weight(1f)
+                    .background(color = Color.Gray)
+            )
+
+            Text(
+                text = "또는 다른 서비스 계정으로 가입",
+                color = Color.Gray,
+                fontSize = 12.sp,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(0.5.dp)
+                    .weight(1f)
+                    .background(color = Color.Gray)
+            )
+        }
+
+        Row(
+            modifier = commonModifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(horizontal = 40.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally)
+        ) {
+            for (type in AccountType.entries) {
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                        .background(color = type.color)
+                )
+            }
+        }
+
+        Row {
+            Text(text = buildAnnotatedString { append(bullet) })
+            Text(text = "")
+        }
+
+        Text(
+            text = buildAnnotatedString {
+                append(bullet)
+                append("\tSNS계정을 간편하게 가입하여 서비스를 이용하실 수 있습니다. 기" +
+                        "\n\t존 POOQ 계정 또는 Wavve 계정과는 연동되지 않으니 이용에 참고" +
+                        "\n\t하세요.")
+            },
+            color = Color.Gray,
+            fontSize = 12.sp,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+
+
+
+        Spacer(Modifier.weight(1f))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.DarkGray)
+                .padding(vertical = 20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Wavve 회원가입", color = Color.White)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
+}
