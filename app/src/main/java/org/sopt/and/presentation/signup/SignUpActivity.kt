ad482f8387e6232b326dc81b1c367cc3c6b8d5d0
boiledegg <tjrwns2163@naver.com>
2024-10-08 00:32:41 +0900

    add/#3: SignUpActivity 생성

---
+++ app/src/main/java/org/sopt/and/presentation/signup/SignUpActivity.kt
@@ -0,0 +1,38 @@
+package org.sopt.and.presentation.signup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.sopt.and.core.designsystem.component.topbar.CancelTopBar
+import org.sopt.and.core.designsystem.theme.ANDANDROIDTheme
+
+class SignUpActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANDANDROIDTheme {
+                Scaffold(
+                    topBar = {
+                        CancelTopBar(
+                            title = "회원가입",
+                            onBackClick = {}
+                        )
+                    }
+                ) { innerPadding ->
+                    SignUpScreen(
+                        modifier = Modifier
+                            .background(color = Color.Black)
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
