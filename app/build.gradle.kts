c9fa743ebddf0352d88fb15b368d1a0410298253
boiledegg <tjrwns2163@naver.com>
2024-10-08 00:42:27 +0900

    add/#3: 뷰모델 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //viewmodel
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
 }
