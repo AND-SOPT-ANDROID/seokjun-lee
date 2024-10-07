bc0885ab3344eeb583cb018648327bce829f1e21
boiledegg <tjrwns2163@naver.com>
2024-10-08 02:49:44 +0900

    add/#3: 내부 저장소 모듈 추가

---
+++ app/src/main/java/org/sopt/and/core/preference/PreferenceModule.kt
@@ -0,0 +1,19 @@
+package org.sopt.and.core.preference
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PreferenceModule {
+    @Provides
+    @Singleton
+    fun providePreferenceUtil(
+        @ApplicationContext context: Context
+    ) = PreferenceUtil(context)
+}
