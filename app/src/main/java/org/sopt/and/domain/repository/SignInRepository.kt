271c98725536702472ff92ea1e9a873c440f3519
boiledegg <tjrwns2163@naver.com>
2024-10-09 02:43:00 +0900

    DEL/#3: 불필요 코드 삭제

--- app/src/main/java/org/sopt/and/domain/repository/SignInRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.and.domain.repository
-
-interface SignInRepository {
-    fun getIdFromPref(): String
-    fun setIdInPreference(id: String)
-    fun getPasswordFromPref(): String
-    fun setPasswordInPreference(password: String)
-}
