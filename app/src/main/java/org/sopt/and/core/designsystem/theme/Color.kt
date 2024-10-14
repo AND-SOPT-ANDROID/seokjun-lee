9baeb18af9d261bbdee9e19ec0207e88fea98c7a
boiledegg <tjrwns2163@naver.com>
2024-10-14 23:39:59 +0900

    add/#5: 색상 추가

--- app/src/main/java/org/sopt/and/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/sopt/and/core/designsystem/theme/Color.kt
@@ -1,6 +1,9 @@
 package org.sopt.and.core.designsystem.theme
 
+import android.content.Context
 import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import org.sopt.and.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +12,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Grey Scale
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFF5F5F5)
+val Grey150 = Color(0xFFE9E9E9)
+val Grey200 = Color(0xFFDDDDDD)
+val Grey300 = Color(0xFFBCBCBC)
+val Grey350 = Color(0xFFADADAD)
+val Grey400 = Color(0xFF666666)
+val Grey500 = Color(0xFF373737)
+val Black = Color(0xFF171717)
