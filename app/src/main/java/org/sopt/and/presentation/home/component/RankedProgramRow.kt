22b28d64d98ee76cf7765db40b8a23db01d0d72f
boiledegg <tjrwns2163@naver.com>
2024-11-01 20:24:31 +0900

    chore/#7: 순위 텍스트 스타일 수정

--- app/src/main/java/org/sopt/and/presentation/home/component/RankedProgramRow.kt
+++ app/src/main/java/org/sopt/and/presentation/home/component/RankedProgramRow.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -90,9 +91,11 @@ fun RankedProgramRow(
 
                     Text(
                         text = (index + 1).toString(),
-                        modifier = Modifier.align(Alignment.BottomStart),
+                        modifier = Modifier.align(Alignment.BottomStart).padding(start = 5.dp),
                         fontSize = 50.sp,
-                        color = White
+                        color = White,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic
                     )
                 }
             }
