9557f67162cd3dfe1ea126bf1665816766ff1c8c
boiledegg <tjrwns2163@naver.com>
2024-11-05 02:57:59 +0900

    del/#9: 라이브러리 정리

--- app/src/main/java/org/sopt/and/data/mapper/SignInMapper.kt
+++ app/src/main/java/org/sopt/and/data/mapper/SignInMapper.kt
@@ -1,7 +1,6 @@
 package org.sopt.and.data.mapper
 
 import org.sopt.and.data.dto.request.SignInRequestDto
-import org.sopt.and.data.dto.request.SignUpRequestDto
 import org.sopt.and.domain.entity.User
 
 fun User.toSignInRequest(): SignInRequestDto = SignInRequestDto(
--- app/src/main/java/org/sopt/and/domain/repository/SignInRepository.kt
+++ app/src/main/java/org/sopt/and/domain/repository/SignInRepository.kt
@@ -2,7 +2,6 @@ package org.sopt.and.domain.repository
 
 import org.sopt.and.data.dto.BaseResponse
 import org.sopt.and.data.dto.response.SignInResponseDto
-import org.sopt.and.data.dto.response.SignUpResponseDto
 import org.sopt.and.domain.entity.User
 import retrofit2.Call
 
